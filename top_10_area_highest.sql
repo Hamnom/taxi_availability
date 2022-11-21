af0a9375e20b09ea6f5d21983b7ac8ba48ac28a1
Hamnom <hamnarashid@hotmail.com>
2022-11-22 01:11:49 +0800

    Update top_10_area_highest.sql

---
+++ top_10_area_highest.sql
@@ -0,0 +1,25 @@
+WITH
+  count_taxis_temp AS (
+  SELECT
+    COUNT(*) AS count_taxi,
+    area,
+    coordinates,
+    'Singapore' AS country,
+  FROM
+    `nth-boulder-368917.love_bonito_data_lake.cord_location`
+  WHERE
+    DATE(_PARTITIONTIME) IS NOT NULL
+  GROUP BY
+    area,
+    coordinates
+  ORDER BY
+    1 DESC)
+SELECT
+  count_taxi,
+  area,
+  coordinates,
+  ROW_NUMBER() OVER (PARTITION BY country ORDER BY count_taxi desc) AS rank
+FROM
+  count_taxis_temp QUALIFY ROW_NUMBER() OVER (PARTITION BY country ORDER BY count_taxi desc) <= 10
+ORDER BY
+  count_taxi desc
